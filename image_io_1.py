5766ddbbe47e0dba291a88dcd41a5b164d3fefd1
suhyun <myksh0903@naver.com>
2022-01-25 14:19:44 +0900

    주석 수정

--- image_io_1.py
+++ image_io_1.py
@@ -2,7 +2,9 @@ import cv2
 
 
 if __name__ == '__main__':
-    color_img = cv2.imread("./img/Lenna.png", cv2.IMREAD_COLOR) #flag : 컬러/흑백으로 읽을 것인지 설정
+    #이미지 읽어오기 - cv2.imread(filename, flag)
+    #flag : 컬러/흑백으로 읽을 것인지 설정
+    color_img = cv2.imread("./img/Lenna.png", cv2.IMREAD_COLOR) 
     gray_img = cv2.imread("./img/Lenna.png", cv2.IMREAD_GRAYSCALE)
     
     print(color_img.shape) #opencv에서는 numpy array 형식으로 표현됨 - shape 확인 가능
@@ -10,7 +12,7 @@ if __name__ == '__main__':
     # (512, 512, 3)
     # (512, 512)     
     
-    #파일로 저장하기
+    #파일로 저장하기 - cv2.imwrite(filename, image var)
     if cv2.imwrite("./img/lenna.jpg", gray_img):
         print("저장완료")
     else:
